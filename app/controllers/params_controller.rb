b6ff4de58d2b7e0a034a9081f41ca57f5a1a0ca0
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-05-31 10:53:18 -0500

    added numeric check to name entered via params

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -2,5 +2,9 @@ class ParamsController < ApplicationController
 
   def example
     @name = params[:name]
+    if @name.start_with?("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+      @invalid = "This does not appear to be a valid name"
+    end
+      
   end
 end
